926a696f3418025f61d9b69af54d02472dbf869f
ggaier <jwenbo52@gmail.com>
2019-04-28 22:52:42 +0800

    stringstream

--- source/basic_3_io.cpp
+++ source/basic_3_io.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -9,7 +12,7 @@ int main(int argc, char const *argv[])
 
     cout << "This "
          << " is a "
-         << "single C++ statement";
+         << "single C++ statement \n";
 
     //标准的 io 中, 除了可以使用 \n 来表示新行外, 还可以是用 endl
     //操作符表示.
@@ -17,8 +20,32 @@ int main(int argc, char const *argv[])
 
     //cin 是默认的输入设备流.
     int i ;
-    cout << "Please enter an integer: ";
-    cin >> i;
-    cout << " and its double value is " << i*2 << ".\n";
+    // cout << "Please enter an integer: ";
+    // cin >> i;
+    // cout << " and its double value is " << i*2 << ".\n";
+
+    //cin and strings, and get line.
+    string mystring;
+    cout << "what's your name? ";
+    getline(cin, mystring);
+    cout << "Hello "<< mystring << ".\n";
+    cout << "What's your favorite team? ";
+    getline(cin, mystring);
+    cout << "I like " << mystring << " too! \n";
+
+    //stringsteam, is most useful to convert strings to numerical values and vice versa.
+    string mystr("1024");
+    int myint;
+    stringstream(mystr) >> myint;
+
+    float price =0;
+    int quantity = 0;
+    cout << "Enter price: ";
+    getline(cin, mystr);
+    stringstream(mystr) >> price;
+    cout << "Enter quantity: ";
+    getline(cin, mystr);
+    stringstream(mystr) >> quantity;
+    cout << "Total price: " << price*quantity << endl;
     return 0;
 }
