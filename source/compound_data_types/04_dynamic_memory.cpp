457985c87a23cd5ef8b96b88e22aaab78975a33a
ggaier <jwenbo52@gmail.com>
2019-05-08 23:14:24 +0800

    dynamic memory

--- source/compound_data_types/04_dynamic_memory.cpp
+++ source/compound_data_types/04_dynamic_memory.cpp
@@ -6,6 +6,25 @@ int main(int argc, char const *argv[])
 {
     //由于某些情况下在运行时才能确定内存的大小, 这就需要
     //动态的申请内存. 也就是 new delete 操作符.
+    int i, n;
+    int *p;
+    cout << "How many numbers would you like to type? ";
+    cin >> i;
+    p = new (nothrow) int[i];
+    if (p == nullptr)
+        cout << "Error: memory could not be allocated";
+    else
+    {
+        for (n = 0; n < i; n++)
+        {
+            cout << "Enter number: ";
+            cin >> p[n];
+        }
+        cout << "You have entered: ";
+        for (n = 0; n < i; n++)
+            cout << p[n] << ", ";
+        delete[] p;
+    }
 
     return 0;
 }
