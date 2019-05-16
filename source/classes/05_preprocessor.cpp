d581bf1c772d99f8e1cb2f1d9dc0fabe857237e4
ggaier <jwenbo52@gmail.com>
2019-05-16 23:35:21 +0800

    宏定义

--- source/classes/05_preprocessor.cpp
+++ source/classes/05_preprocessor.cpp
@@ -7,6 +7,19 @@
 /*还可以定义方法宏*/
 #define getmax(a, b) (a > b ? a : b)
 
+/*宏的生命周期是当有#undef 出现的时候
+例如: #undef TABLESIZE.
+*/
+
+/*
+方法宏中, 有两个操作符 #, 和 ## . 操作符要有空格.
+前者#, 表示把一个变量加双引号
+后者## 表示把前后两个变量无空格连接在一起.
+*/
+#define str(r) # r
+
+#define glue(a, b) a ## b
+
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -22,6 +35,10 @@ int main(int argc, char const *argv[])
     y = getmax(x, 2);
     cout << y << endl;
     cout << getmax(7, x) << endl;
+
+    //#, 和##
+    cout << str(test) << endl;
+    glue(c, out) << "test" << endl;
 
     return 0;
 }
